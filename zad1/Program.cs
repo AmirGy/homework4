59c0be4cf940da5e6ae3c0714f1148e06a818599
Amir <firstharrypotter1@gmail.com>
2022-12-11 18:21:46 +0600

    solved zad2

---
+++ zad1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int DegreeNum(int arg1, int arg2)
+{
+    int T = A;
+    for (int i = 1; i < B; i++)
+    {
+        A = A * T;
+    }
+    return A;
+}
+
+Console.Write(DegreeNum(A, B));
---
+++ zad2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Через строку решать нельзя.
+// 452 -> 11 82 -> 10 9012 -> 12
+
+Console.Write("Введмте число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int SumNum(int arg)
+{
+    int sum = 0;
+    while (num >= 1)
+    {
+        int last = num % 10;
+        sum = sum + last;
+        num = num / 10;
+    }
+    return sum;
+}
+
+Console.Write(SumNum(num));
